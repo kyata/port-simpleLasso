968d08fade791dc77c627b17976c366afa4775ad
Yuki Katayama <y.ktym.1986@gmail.com>
2018-07-07 22:41:27 +0900

    浮動小数点一致判定を近似値で行うように変更

--- tests/test_cd.cc
+++ tests/test_cd.cc
@@ -30,12 +30,13 @@ TEST_F(TestSimpleLasso, getSignTest)
 
 TEST_F(TestSimpleLasso, innerProductTest)
 {
-    std::vector<double> vec {
-        0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0
-    };
+    std::vector<double> v1  = getVector(lhs_, 0, GET_VECTOR_TYPE_COL);
+    std::vector<double> v2  = getVector(lhs_, 1, GET_VECTOR_TYPE_COL);
 
-    double prod = innerProduct(vec, vec);
-    EXPECT_EQ(285.0, prod);
+    double prod1 = innerProduct(v1, v2);
+    double prod2 = lhs_.col(0).dot( lhs_.col(1) );
+
+    EXPECT_FLOAT_EQ(prod1, prod2);
 }
 
 TEST_F(TestSimpleLasso, getVectorTest)
@@ -46,11 +47,11 @@ TEST_F(TestSimpleLasso, getVectorTest)
     std::vector<double> vec2 = getVector(lhs_, idx, GET_VECTOR_TYPE_ROW);
 
     for( auto i=0; i<vec1.size(); i++) {
-        EXPECT_EQ( vec1[i], lhs_(i, idx) );
+        EXPECT_DOUBLE_EQ( vec1[i], lhs_(i, idx) );
     }
 
     for(auto i=0; i<vec2.size(); i++) {
-        EXPECT_EQ( vec2[i], lhs_(idx, i) );
+        EXPECT_DOUBLE_EQ( vec2[i], lhs_(idx, i) );
     }
 }
 
